78735ad56fdb435020e9ac11781d3e75a4f563eb
dhuharamadhani <dhuharamadhani@protonmail.com>
2024-06-12 04:46:14 +0700

    Deleting post and images, update menu name

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
+  { href: '/blog', title: 'Archives' },
   { href: '/tags', title: 'Tags' },
   { href: '/works', title: 'Works' },
   { href: '/bio', title: 'Bio' },
