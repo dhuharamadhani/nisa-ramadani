8f872e1ab8f47fca0214dbccacd550e9c00e3f7f
dhuharamadhani <dhuharamadhani@protonmail.com>
2024-06-11 17:36:36 +0700

    Update layout

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,11 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/works', title: 'Works' },
-  { href: '/bio', title: 'Bio' },
+  { href: '/blog', title: 'Archives' },
+  { href: '/bio', title: 'Biography' },
 ]
 
 export default headerNavLinks
+
+{/*{ href: '/works', title: 'Works' },
+{ href: '/tags', title: 'Tags' },
+*/}
