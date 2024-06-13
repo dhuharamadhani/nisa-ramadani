be913b878da007515d802d5faf5ca3c5a616432b
dhuharamadhani <dhuharamadhani@protonmail.com>
2024-06-13 17:00:12 +0700

    Edit home and mobile nav

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,10 +1,11 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/archive', title: 'Archives' },
-  { href: '/tags', title: 'Tags' },
+  
   { href: '/sketches', title: 'Sketches' },
   { href: '/bio', title: 'Biography' },
 ]
 
 export default headerNavLinks
 {/*   { href: '/works', title: 'Works' }, */}
+{/* { href: '/tags', title: 'Tags' }, */}
--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -2,6 +2,10 @@ import { ReactNode } from 'react'
 import type { Authors } from 'contentlayer/generated'
 import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
+import tagData from 'app/tag-data.json'
+import Link from '@/components/Link'
+import Tag from '@/components/Tag'
+import { slug } from 'github-slugger'
 
 interface Props {
   children: ReactNode
@@ -10,7 +14,9 @@ interface Props {
 
 export default function AuthorLayout({ children, content }: Props) {
   const { name, avatar, occupation, company, email, instagram, linkedin, github } = content
-
+  const tagCounts = tagData as Record<string, number>
+  const tagKeys = Object.keys(tagCounts)
+  const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
@@ -43,6 +49,31 @@ export default function AuthorLayout({ children, content }: Props) {
           </div>
         </div>
       </div>
+      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
+        <div className="space-x-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-3xl md:leading-14">
+            Forms & Topics
+          </h1>
+        </div>
+        <div className="flex max-w-lg flex-wrap">
+          {tagKeys.length === 0 && 'No tags found.'}
+          {sortedTags.map((t) => {
+            return (
+              <div key={t} className="mb-2 mr-5 mt-2">
+                <Tag text={t} />
+                <Link
+                  href={`/tags/${slug(t)}`}
+                  className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
+                  aria-label={`View posts tagged ${t}`}
+                >
+                  {` (${tagCounts[t]})`}
+                </Link>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
     </>
   )
 }
