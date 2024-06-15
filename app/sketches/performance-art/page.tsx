1285b2cb30f789dde32d243d0f67303c83acc17d
dhuharamadhani <dhuharamadhani@protonmail.com>
2024-06-15 23:25:28 +0700

    Add place-content-center on sketches page

--- app/sketches/performance-art/page.tsx
+++ app/sketches/performance-art/page.tsx
@@ -14,7 +14,7 @@ export default function PerformanceArtSketches() {
           </h1>
         </div>
         <div className="container py-12">
-          <div className="-m-4 flex flex-wrap">
+          <div className="place-content-center -m-4 flex flex-wrap">
             {performanceArtSketchesData.map((d) => (
               <SketchesCard
                 key={d.title}
